98f9aa57c212f434e6e98c2fd29d8d328f1c7af9
KaleemAhmed123 <shaamidreez@gmail.com>
2026-01-25 15:45:08 +0530

    Updating SocialMedia and Header data change.

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,25 +1,46 @@
-import React from "react";
-import { BsTwitter } from "react-icons/bs";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { BsTwitter, BsGithub, BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs";
+import { SiLeetcode, SiStackoverflow } from "react-icons/si";
+import { client } from "../client";
 
-const SocialMedia = () => (
-  <div className="app__social">
-    <div>
-      <a href="https://twitter.com/IdreesShaam" target="__blank">
-        <BsTwitter />
-      </a>
+const SocialMedia = () => {
+  const [socialLinks, setSocialLinks] = useState([]);
+
+  useEffect(() => {
+    // Fetch social links, ordered by 'order' field
+    const query = '*[_type == "socialLinks"] | order(order asc)';
+
+    client.fetch(query).then((data) => {
+      setSocialLinks(data);
+    });
+  }, []);
+
+  // Icon mapping for social platforms
+  const getIcon = (platform) => {
+    const icons = {
+      linkedin: BsLinkedin,
+      github: BsGithub,
+      twitter: BsTwitter,
+      stackoverflow: SiStackoverflow,
+      leetcode: SiLeetcode,
+      facebook: BsFacebook,
+      instagram: BsInstagram,
+    };
+    const IconComponent = icons[platform.toLowerCase()] || BsGithub;
+    return <IconComponent />;
+  };
+
+  return (
+    <div className="app__social">
+      {socialLinks.map((link, index) => (
+        <div key={index}>
+          <a href={link.url} target="__blank" rel="noopener noreferrer">
+            {getIcon(link.platform)}
+          </a>
+        </div>
+      ))}
     </div>
-    <div>
-      <a href="https://www.linkedin.com/in/kaleem-ahmed-/" target="__blank">
-        <FaLinkedinIn />
-      </a>
-    </div>
-    <div>
-      <a href="https://github.com/KaleemAhmed123" target="__blank">
-        <FaGithub />
-      </a>
-    </div>
-  </div>
-);
+  );
+};
 
 export default SocialMedia;
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,98 +1,128 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./Header.scss";
-import { BsGithub, BsLinkedin, BsTwitter } from "react-icons/bs";
-import { SiLeetcode } from "react-icons/si";
+import { BsGithub, BsLinkedin, BsTwitter, BsFacebook, BsInstagram } from "react-icons/bs";
+import { SiLeetcode, SiStackoverflow } from "react-icons/si";
 import { AppWrap } from "../../wrapper";
+import { client } from "../../client";
 
-const Header = () => (
-  <motion.div
-    whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-    transition={{ duration: 0.5 }}
-    className="container"
-  >
-    <div className="row">
-      <div className="left">
-        <ul className="info">
-          <li>
-            <span className="first-block">Full Name:</span>
-            <span className="second-block">Kaleem Ahmed</span>
-          </li>
-          <li>
-            <span className="first-block">Phone:</span>
-            <a href="tel:+91 6306026859" target="__blank">
-              <span className="second-block">+91 6306026859</span>
-            </a>
-          </li>
-          <li>
-            <span className="first-block">Email:</span>
-            <a href="mailto:shaamidreez@gmail.com" target="__blank">
-              <span className="second-block">shaamidreez@gmai.com</span>
-            </a>
-          </li>
-          <li>
-            <span className="first-block">Website:</span>
-            <a href="https://kaleem.netlify.app" target="__blank">
-              <span className="second-block">kaleem.netlify.com</span>
-            </a>
-          </li>
-          <li>
-            <span className="first-block">Location:</span>
-            <span className="second-block">UP India 208019</span>
-          </li>
-        </ul>
+const Header = () => {
+  const [personalInfo, setPersonalInfo] = useState(null);
+  const [intro, setIntro] = useState(null);
+  const [socialLinks, setSocialLinks] = useState([]);
+
+  useEffect(() => {
+    // Fetch personal info
+    const personalInfoQuery = '*[_type == "personalInfo"][0]';
+    client.fetch(personalInfoQuery).then((data) => {
+      setPersonalInfo(data);
+    });
+
+    // Fetch intro
+    const introQuery = '*[_type == "intro"][0]';
+    client.fetch(introQuery).then((data) => {
+      setIntro(data);
+    });
+
+    // Fetch social links, ordered by 'order' field
+    const socialLinksQuery = '*[_type == "socialLinks"] | order(order asc)';
+    client.fetch(socialLinksQuery).then((data) => {
+      setSocialLinks(data);
+    });
+  }, []);
+
+  // Icon mapping for social platforms
+  const getIcon = (platform) => {
+    const icons = {
+      linkedin: BsLinkedin,
+      github: BsGithub,
+      twitter: BsTwitter,
+      stackoverflow: SiStackoverflow,
+      leetcode: SiLeetcode,
+      facebook: BsFacebook,
+      instagram: BsInstagram,
+    };
+    const IconComponent = icons[platform.toLowerCase()] || BsGithub;
+    return <IconComponent />;
+  };
+
+  return (
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="container"
+    >
+      <div className="row">
+        <div className="left">
+          <ul className="info">
+            {personalInfo && (
+              <>
+                {personalInfo.fullName && (
+                  <li>
+                    <span className="first-block">Full Name:</span>
+                    <span className="second-block">{personalInfo.fullName}</span>
+                  </li>
+                )}
+                {personalInfo.phone && (
+                  <li>
+                    <span className="first-block">Phone:</span>
+                    <a href={`tel:${personalInfo.phone}`} target="__blank">
+                      <span className="second-block">{personalInfo.phone}</span>
+                    </a>
+                  </li>
+                )}
+                {personalInfo.email && (
+                  <li>
+                    <span className="first-block">Email:</span>
+                    <a href={`mailto:${personalInfo.email}`} target="__blank">
+                      <span className="second-block">{personalInfo.email}</span>
+                    </a>
+                  </li>
+                )}
+                {personalInfo.website && (
+                  <li>
+                    <span className="first-block">Website:</span>
+                    <a href={personalInfo.website} target="__blank">
+                      <span className="second-block">
+                        {personalInfo.website.replace(/^https?:\/\//, '')}
+                      </span>
+                    </a>
+                  </li>
+                )}
+                {personalInfo.location && (
+                  <li>
+                    <span className="first-block">Location:</span>
+                    <span className="second-block">{personalInfo.location}</span>
+                  </li>
+                )}
+              </>
+            )}
+          </ul>
+        </div>
+        <div className="right">
+          {intro && (
+            <>
+              {intro.greeting && <h2>{intro.greeting}</h2>}
+              {intro.section1 && <p>{intro.section1}</p>}
+              {intro.section2 && <p>{intro.section2}</p>}
+              {intro.section3 && <p>{intro.section3}</p>}
+            </>
+          )}
+          {socialLinks.length > 0 && (
+            <ul className="icons">
+              {socialLinks.map((link, index) => (
+                <li key={index}>
+                  <a href={link.url} target="__blank" rel="noopener noreferrer">
+                    {getIcon(link.platform)}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-      <div className="right">
-        <h2>Hello There!</h2>
-        <p>
-          I'm Kaleem, I have sound knowledge of Data Structures and Algorithms.
-          With a passion for problem-solving, I've solved <b>1100+</b> coding
-          problems in the past 2.5 years on various platforms like{" "}
-          <b>Leetcode</b>, <b>GFG</b>, <b>Codeforces</b>. I stay up to date with
-          the latest industry trends to tackle complex problems and provide
-          creative solutions that impress my peers and mentors alike. &nbsp;
-        </p>
-        <p>
-          And I possess a wide range of technical skills and have demonstrated
-          proficiency in web technologies, I've also gained considerable
-          experience building projects using latest technologies, which has
-          helped me develop my problem-solving skills and hone my ability to
-          work in a team.
-        </p>
-        <p>
-          Overall, I'm confident in my technical abilities and eager to leverage
-          my skills to contribute to meaningful projects in the future. Whether
-          it's building scalable web applications, designing efficient
-          algorithms, or collaborating with teams, I am excited to take on new
-          challenges and grow as a developer.
-        </p>
-        <ul className="icons">
-          <li>
-            <a
-              href="https://www.linkedin.com/in/kaleem-ahmed-"
-              target="__blank"
-            >
-              <BsLinkedin />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/KaleemAhmed123" target="__blank">
-              <BsGithub />
-            </a>
-          </li>
-          <li>
-            <a href="https://leetcode.com/Kaleem_Ahmed/" target="__blank">
-              <SiLeetcode />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/IdreesShaam" target="__blank">
-              <BsTwitter />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </motion.div>
-);
+    </motion.div>
+  );
+};
+
 export default AppWrap(Header, "home");
