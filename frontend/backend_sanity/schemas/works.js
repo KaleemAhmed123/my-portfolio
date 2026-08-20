6e9682dd059f437706f5e044aed7058459c53e6e
KaleemAhmed123 <shaamidreez@gmail.com>
2026-08-20 12:10:40 +0530

    Add an order field so abouts, skills and projects can be sorted from the cms

--- frontend/backend_sanity/schemas/works.js
+++ frontend/backend_sanity/schemas/works.js
@@ -4,6 +4,12 @@ export default {
     type: 'document',
     fields: [
       {
+        name: 'order',
+        title: 'Order',
+        type: 'number',
+        description: 'Lower numbers show first.',
+      },
+      {
         name: 'title',
         title: 'Title',
         type: 'string',
